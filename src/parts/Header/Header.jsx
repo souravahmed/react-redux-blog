e009e7acd13b4acc386fbbb8836aef33850f54d8
Sourav Ahmed <souravahmeddiu@gmail.com>
2021-06-08 16:26:29 +0600

    root path set to project name

--- src/parts/Header/Header.jsx
+++ src/parts/Header/Header.jsx
@@ -49,7 +49,7 @@ const index = () => {
 
       <div className="container">
         <Switch>
-          <Route exact path="/">
+          <Route exact path="/react-redux-blog">
             <HomePage />
           </Route>
           <Route exact path="/users">
